ef6f87d87e988b8b979315a156d98b14f5ce2c99
Nichole <nicholesvibrantlife@gmail.com>
2018-02-19 00:22:09 -0800

    sql query updated to join, and add filter to remove dates greater than 15 years ago

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const sqlite = require('sqlite'),
+'use strict';
+
+const sqlite3 = require('sqlite3').verbose(),
+	  db = new sqlite3.Database('./db/database.db'),
       Sequelize = require('sequelize'),
       request = require('request'),
       express = require('express'),
+      // filmRoutes = require('routes');      
       app = express();
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
@@ -14,9 +18,33 @@ Promise.resolve()
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
+
+// SQL
+let sqlJoin = "SELECT films.id id, films.title title, films.release_date release_date, genres.name name from films inner join genres on genres.id = films.genre_id WHERE release_date BETWEEN datetime('now', '-15 years') AND datetime('now', 'localtime' )";
+
+
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  // res.status(500).send('You are the best');
+	db.serialize(function() {
+		
+		
+	});
+
+	db.all(sqlJoin, [], (err, rows) => {
+		if (err){
+			console.log(err.message);
+		}
+		let hash = {};
+		hash['recommendations'] = rows;
+		res.json(hash);
+	})
+
+
 }
+// database 
+
+
+
 
 module.exports = app;
--- routes.js
+++ routes.js
@@ -1,12 +1,1 @@
-'use strict';
-
-module.exports = function(app)
-{
-	var films = require('controller');
-
-	app.route('/films')
-		.get(films.list_all_films)
-
-	app.route('/films/:filmId')
-		.get(films.read_film)
-}
+routes.js
